bc6017579da25ceeab067c17a809d8d084f87bd4
Perry Chien <peiruchien1@gmail.com>
2024-09-25 01:51:34 -0700

    Refactored cam_calib to be callable by script

--- makeshiftStereoCam/src/cam_calibration.py
+++ makeshiftStereoCam/src/cam_calibration.py
@@ -1,48 +1,36 @@
+# cam_calibration.py
 # Perry Chien, Husky Robotics, PY 2024
-# Last updated 3/7/24
+# Performs camera calibration to obtain distortion parameters.
 
-# This method handles the camera calibration part of the makeshift stereo
-# camera (STEP 1). This part of the workflow is used to find the distortion
-# parameters of the Mastcam module. Once calibrated using an algorithm that
-# looks for known object and image points (i.e. chessboard corners), these
-# calibration parameters can be fed into STEP 2, the triangulation algo to
-# estimate depth using two views captured by the single Mastcam.
-
-import numpy as np
 import cv2 as cv
+import numpy as np
 import glob
 
-# termination criteria
-criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+def calibrate_camera(save_path='calibration_params.npz'):
+    # Calibrates the camera and saves calibration parameters.
+    # Saves the camera intrinsic parameters and distortion coefficients.
+    
+    criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+    objp = np.zeros((6*7,3), np.float32)
+    objp[:,:2] = np.mgrid[0:7,0:6].T.reshape(-1,2)
 
-# prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
-objp = np.zeros((6*7,3), np.float32)
-objp[:,:2] = np.mgrid[0:7,0:6].T.reshape(-1,2)
+    objpoints = []  # 3D points in real world space
+    imgpoints = []  # 2D points in image plane
 
-# Arrays to store object points and image points from all the images.
-objpoints = [] # 3d point in real world space
-imgpoints = [] # 2d points in image plane.
+    images = glob.glob('*.jpg')
+    for fname in images:
+        img = cv.imread(fname)
+        gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+        ret, corners = cv.findChessboardCorners(gray, (7,6), None)
+        if ret:
+            objpoints.append(objp)
+            corners2 = cv.cornerSubPix(gray, corners, (11,11), (-1,-1), criteria)
+            imgpoints.append(corners2)
+            cv.drawChessboardCorners(img, (7,6), corners2, ret)
+            cv.imshow('img', img)
+            cv.waitKey(500)
+    cv.destroyAllWindows()
 
-# new glob
-images = glob.glob('*.jpg')
-for fname in images:
-    img = cv.imread(fname)
-    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-
-    # Find the chess board corners
-    ret, corners = cv.findChessboardCorners(gray, (7,6), None)
-
-    # If found, add object points, image points (after refining them)
-    if ret == True:
-        objpoints.append(objp)
-        corners2 = cv.cornerSubPix(gray,corners, (11,11), (-1,-1), criteria)
-        imgpoints.append(corners2)
-
-        # Draw and display the corners
-        cv.drawChessboardCorners(img, (7,6), corners2, ret)
-        cv.imshow('img', img)
-        cv.waitKey(500)
-cv.destroyAllWindows()
-
-# Calibration using the generated objpoints and imgpoints
-ret, mtx, dist, rvecs, tvecs = cv.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
+    ret, mtx, dist, rvecs, tvecs = cv.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
+    np.savez(save_path, mtx=mtx, dist=dist, rvecs=rvecs, tvecs=tvecs)
+    print(f"Calibration parameters saved to {save_path}")
