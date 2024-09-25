e3ac8fa6aea8ba72546eb48be96bcccf1a964765
Perry Chien <peiruchien1@gmail.com>
2024-09-25 02:30:03 -0700

    More accurate commenting for main script

--- makeshiftStereoCam/scripts/main.py
+++ makeshiftStereoCam/scripts/main.py
@@ -7,16 +7,18 @@ from src.stereo_depth_estimation import compute_depth
 from src.image_capture import capture_images
 
 def main():
-    # Coordinates the entire process from calibration to depth estimation.
-    
+
+    # Step 1, cam calibration 
     if input("Run camera calibration? (yes/no): ").strip().lower() == 'yes':
         calibrate_camera()
 
+    # Step 2, time-delay image capturing (2 image pair)
     img_left, img_right = capture_images()
     if img_left is None or img_right is None:
         print("Error in capturing images. Exiting...")
         return
 
+    # Step 3, depth estimation via simulated stereo vision
     depth_map, average_depth = compute_depth('captures/left_image.jpg', 'captures/right_image.jpg')
     print(f"Estimated depth at center location: {average_depth:.2f} meters")
 
